020894ff437ade5e2474495884a3544d8b075cfa
DengGuangRong <13627118531@163.com>
2017-06-06 11:17:54 +0800

    Create New File Demo.java

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello Git!!!") ;
+		System.out.println("认真听课的孩子是好孩子!!!") ;
+		System.out.println("不认真听课的孩纸更是好孩子") ;
+		System.out.println("又听课又不听课的孩纸是最好的孩子") ;
+	}
+} 
